872a99ec59b4db01ec56d905560577c2ed71ba1d
Lucas <lucasbo.dev@gmail.com>
2025-08-14 14:28:54 -0300

    feat: adiciona os endpoints e suas rotas no controller de missão com mock

--- src/main/java/dev/java10x/CadastroDeNinjas/Controller/MissaoController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Controller/MissaoController.java
@@ -1,4 +1,28 @@
 package dev.java10x.CadastroDeNinjas.Controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/missoes")
 public class MissaoController {
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missão criada.";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missão alterada";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missão deletada";
+    }
+
+    @PostMapping("/listar")
+    public String listarMissao(){
+        return "Lista das missões:";
+    }
 }
