460ba56bd70bfbae0f309759748095df0c0b32a2
Lucas <lucasbo.dev@gmail.com>
2025-08-20 14:54:35 -0300

    refactor: cria os DTOs para as entidades

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/DTO/NinjaMapper.java
@@ -0,0 +1,30 @@
+package dev.java10x.CadastroDeNinjas.DTO;
+
+import dev.java10x.CadastroDeNinjas.Model.NinjaModel;
+import org.springframework.stereotype.Component;
+
+@Component
+@Mapper
+public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO){
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel){
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        return ninjaDTO;
+
+    }
+}
--- src/main/java/dev/java10x/CadastroDeNinjas/DTO/ninjaDTO.java
+++
@@ -1,4 +0,0 @@
-package dev.java10x.CadastroDeNinjas.DTO;
-
-public class ninjaDTO {
-}
